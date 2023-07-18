103a1eb9283228e4b66b1982dde9be8625df793c
jagan <jagannj58@gmail.com>
2023-07-18 11:23:45 +0530

    user crud-Completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,19 +25,69 @@ exports.UserLogin = asynchandler(async(req,res)=>{
   // User  if user exists or Not 
   const findUser = await User.findOne({email:email});
   if(findUser && await findUser.isPasswordMatched(password)){
+    
     res.json({
         _id:findUser?._id,
         firstname:findUser?.firstname,
         lastname:findUser?.lastname,
         email: findUser?.email,
         mobile:findUser?.mobile,
-        createdat:findUser?.createdat,
-        token:genToken(findUser?._id,)
+        token:genToken(findUser?._id)
     })
   }
   else{
-    throw new Error(`Invalid User : ${email}`)
+    throw new Error(`Invalid Credentials`)
   }
 
+})
+
+//Get all users
+
+exports.getallUser =asynchandler(async(req,res)=>{
+  try {
+    const getUser =await User.find();
+    res.json(getUser)
+  } catch (error) {
+    throw new Error(error)
+  }
+})
+
+exports.getaUser =asynchandler(async(req,res)=>{
+  try {
+    // res.json()
+    const{id}= req.params;
+    const getUserbyId =await User.findById(id);
+    res.json(getUserbyId)
+
+  } catch (error) {
+    throw new Error(error)
+  }
+})
+
+exports.deleteaUser =asynchandler(async(req,res)=>{
+  try {
+    // res.json()
+    const{id}= req.params;
+    const deleteUserbyId =await User.findByIdAndDelete(id);
+    res.json(deleteUserbyId)
+
+  } catch (error) {
+    throw new Error(error)
+  }
+})
+
+
+exports.UpdateaUser =asynchandler(async(req,res)=>{
+  const {firstname,lastname,mobile,email} = req.body;
+  try {
+    // res.json()
+    const{id}= req.params;
+    const updateUser =await User.findByIdAndUpdate(id,{firstname,lastname,mobile,email},
+      {new : true},);
+    res.json(updateUser)
+
+  } catch (error) {
+    throw new Error(error)
+  }
 })
 
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const dotenv = require("dotenv").config();
 const dBconnected = require("./config/dbConnect")
 const authRouters = require("./routes/authRoute")
