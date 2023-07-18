974f63d58b6576c7ad84528299c8ddd6fb12ed43
jagan <jagannj58@gmail.com>
2023-07-18 11:36:16 +0530

    repeat-1

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 exports. notFound = (req,res,next)=>{
     const error = new Error(`Not Found : ${req.originalUrl}`);
     res.status(404),
-    next(error)
+    next(error) 
 }
 
 // Error Handler 
